3f1f20b51cfedee25e5ba34083158703edc0cf33
nishalaha <nalahakone@deakin.edu.au>
2019-08-27 20:38:15 +1000

    no message

---
+++ src/App.js
@@ -0,0 +1,53 @@
+﻿import React from 'react';
+import './App.css';
+import WeatherInfoComponent from './WeatherInfoComponent';
+
+const fetch = require("node-fetch");
+const API_KEY = "ca9418e154204e015d546d486eb2373f"
+
+class App extends React.Component {
+    state = {
+        city : "Melbourne"
+    }
+    handleChanges = (event) =>
+    {
+        this.setState({city:event.target.value})
+    }
+    getWeather = () =>
+    {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${API_KEY}`)
+          .then(response => response.json())
+          .then(data => {
+            this.setState({weather:data})
+        })
+            .catch(error => {
+                console.log(error);
+            })
+
+
+    }
+    render() {
+        return (
+            <div className="App" >
+
+                <header className="App-header">
+                    <label for="city">City</label>
+                    <input id={"City "}
+                        type='text'
+                        value={this.state.city}
+                        onChange={this.handleChanges}/>
+                    <input type="button"
+                        onClick={this.getWeather}
+                        value={'Get Weather'}/>
+                </header>
+                <div className="App-content">
+                    <WeatherInfoComponent weather={this.state.weather}/>
+				</div>
+            </div>
+
+
+            )
+    }
+}
+
+export default App;
---
+++ src/WeatherInfoComponent.js
@@ -0,0 +1,37 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class WeatherInfoComponent extends React.Component {
+	
+	render() {
+		console.log(this.props.weather);
+		if (this.props.weather == null) {
+			return (
+				<React.Fragment>
+				</React.Fragment>
+			);
+		} else {
+			return (
+				<React.Fragment>
+					<h1>Weather in {this.props.weather.name}, {this.props.weather.sys.country}</h1>
+					<p>{parseInt(this.props.weather.main.temp_max - 273.15)}°C | {this.props.weather.weather[0].description} </p>
+					<img src={"https://openweathermap.org/img/wn/" + this.props.weather.weather[0].icon + "@2x.png"} />
+					
+					<p><b>&ensp;Wind Speed</b> &ensp;{this.props.weather.wind.speed}m/s</p>
+					<p><b>Pressure</b> &ensp;{this.props.weather.main.pressure} hpa </p>
+					<p><b>Humidity</b> &ensp; &ensp; &ensp; {this.props.weather.main.humidity}% </p>
+					<p><b>Sunrise</b> &ensp; &ensp; &ensp; {this.props.weather.sys.sunrise} </p>
+					<p><b>Sunset</b> &ensp; &ensp; &ensp; {this.props.weather.sys.sunset} </p>
+					<p><b> &ensp;&ensp;Coord</b>  &ensp;{this.props.weather.coord.lon}, {this.props.weather.coord.lat} </p>
+
+
+				</React.Fragment>
+			);
+		}
+	}
+}
+
+WeatherInfoComponent.propTypes = {
+	weather : PropTypes.object,
+}
+export default WeatherInfoComponent;
